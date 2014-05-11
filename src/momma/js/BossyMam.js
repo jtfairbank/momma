6535d836baa55cc39628c6af8c490a9e518d21aa
jtfairbank <jtfairbank@gmail.com>
2014-05-11 17:33:40 +0200

    Images and momma files.

---
+++ src/momma/js/BossyMam.js
@@ -0,0 +1,9 @@
+/* Momma Controller
+ * ==========================================================================
+ * Interacts with a page to integrate different components:
+ *
+ *   * a form for visitors to interact with the page
+ *   * the eyes jquery plugin which shoes the images
+ */
+
+ // TODO
